ab08ec69025dc2e0f23d938e9ffd256b03d5da17
EDR3X <dhunganaannanta@gmail.com>
2022-02-21 15:44:42 +0545

    hello there

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\tools\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\Tools\\projects\\Flutter\\esewa style floating button\\floatingbutton" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\tools\\flutter"
+  "PROJECT_DIR=D:\\Tools\\projects\\Flutter\\esewa style floating button\\floatingbutton"
+  "FLUTTER_ROOT=C:\\tools\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\Tools\\projects\\Flutter\\esewa style floating button\\floatingbutton\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\Tools\\projects\\Flutter\\esewa style floating button\\floatingbutton"
+  "FLUTTER_TARGET=D:\\Tools\\projects\\Flutter\\esewa style floating button\\floatingbutton\\lib\\main.dart"
+  "DART_DEFINES=Zmx1dHRlci5pbnNwZWN0b3Iuc3RydWN0dXJlZEVycm9ycz10cnVl,RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\Tools\\projects\\Flutter\\esewa style floating button\\floatingbutton\\.dart_tool\\package_config.json"
+)
